55f646688956723c9c37321a21fbd843dcacfbe7
Yoan Thirion <yoan.thirion@gmail.com>
2022-09-14 13:25:30 +0200

    refactoring part 1

--- tripservice-kata/java/3.refactoring/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ tripservice-kata/java/3.refactoring/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -8,22 +8,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TripService {
+    public static final ArrayList<Trip> NO_TRIPS = new ArrayList<>();
+
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<>();
         User loggedUser = getLoggedUser();
-        boolean isFriend = false;
-        if (loggedUser != null) {
-            for (User friend : user.getFriends()) {
-                if (friend.equals(loggedUser)) {
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend) {
-                tripList = findTripsByUser(user);
-            }
-            return tripList;
-        } else {
+        checkUser(loggedUser);
+
+        return user.isFriendWith(loggedUser)
+                ? findTripsByUser(user)
+                : NO_TRIPS;
+    }
+
+    private void checkUser(User loggedUser) {
+        if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
     }
